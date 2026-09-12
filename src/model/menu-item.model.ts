35791e384eeea8562e62f2f448a283cd87dd7376
hatefkhodkar <hatef.khodkar@hotmail.com>
2026-09-12 21:52:59 +0400

    feat: add menu item model

---
+++ src/model/menu-item.model.ts
@@ -0,0 +1,10 @@
+export interface menuItem {
+    id?: string,
+    name: string,
+    description?: string,
+    price: number,
+    category?: string,
+    isAvailable?: boolean,
+    isPopular?: boolean,
+    image?: string,
+}
