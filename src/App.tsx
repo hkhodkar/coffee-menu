6052fe9c7a705e2617ec76dc1c9b99bb6d93985a
hatefkhodkar <hatef.khodkar@hotmail.com>
2026-09-12 19:26:47 +0400

    feat: add Button component to App and update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 function App() {
-  return <h1 className="text-5xl font-bold">Coffee Menu</h1>;
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Coffee Menu</h1>
+      <Button variant={"secondary"}>Coffee Menu</Button>
+    </>
+  );
 }
 
 export default App;
